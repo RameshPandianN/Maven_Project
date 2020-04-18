db706451a43bffe6e119796d5e8cc70fd703c633
Ubuntu <ramesh.pandian@mindtree.com>
2020-04-18 16:23:10 +0000

    added few files

---
+++ src/main/java/test_project/java_prjt/App.java
@@ -0,0 +1,13 @@
+package test_project.java_prjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/test_project/java_prjt/testclass.java
@@ -0,0 +1,16 @@
+package test_project.java_prjt;
+
+public class testclass {
+	
+	public static void main(String args[])
+	
+	{
+		
+		System.out.println("I am learning DEVOPS");
+		int a=50;
+		int b=50;
+		System.out.println(a+ b);
+		System.out.println("I am going to become DEVOPS expert");
+	}
+
+}
